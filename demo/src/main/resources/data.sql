43db01980cf8647ac99b20f976598cdaf1a4a45c
SWAGAT SWAROOP PARIDA <swagatswaroop@gmail.com>
2025-10-12 17:16:31 +0530

    Spring Small Application Done

---
+++ demo/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+
+
+insert into alien (id,name,tech) values (101,'Swagat','Blockchain');
+insert into alien (id,name,tech) values (102,'Aryan','Backend');
---
+++ demo/src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+-- Corrected schema.sql
+CREATE TABLE alien (
+    id INT PRIMARY KEY,
+    name VARCHAR(250),
+    tech VARCHAR(250)
+);
