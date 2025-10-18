4cdd3d08a8e628c8a1973af7c0a1cafab860698e
SWAGAT SWAROOP PARIDA <swagatswaroop@gmail.com>
2025-10-18 15:46:21 +0530

    Now This Project is Done

---
+++ spring-mvc/src/main/java/com/swagat/spring_mvc/HomeController.java
@@ -0,0 +1,13 @@
+package com.swagat.spring_mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String  home(){
+        System.out.println("Home page requested");
+        return "index.jsp";
+    }
+}
