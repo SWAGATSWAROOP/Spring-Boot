c8c58e19841081ca455cf2c039e9dabb1c010271
SWAGAT SWAROOP PARIDA <swagatswaroop@gmail.com>
2025-10-20 02:24:53 +0530

    Done

--- spring-rest/src/main/java/com/swagat/spring_rest/AlienController.java
+++ spring-rest/src/main/java/com/swagat/spring_rest/AlienController.java
@@ -4,27 +4,31 @@ import com.swagat.spring_rest.model.Alien;
 import com.swagat.spring_rest.repo.AlienRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class AlienController {
     @Autowired
     AlienRepo repo;
 
     @GetMapping("aliens")
-    @ResponseBody
     public List<Alien> getAliens(){
         List<Alien> aliens = repo.findALl();
         return aliens;
     }
 
-    @GetMapping("alien/{aid}")
-    @ResponseBody
+    @GetMapping(path = "alien/{aid}",produces = "application/xml") //Returns what type of response
     public Alien getAlien(@PathVariable("aid") int aid){
         return repo.findById(aid).orElse(new Alien());
+    }
+
+//    Accept form data
+    @PostMapping(path = "alien",consumes = {"application/json"}) //This makes only accept json
+//    @RequestBody used for converting json body to Java object format
+    public Alien saveAlien(@RequestBody Alien a){
+        repo.save(a);
+        return a;
     }
 }
